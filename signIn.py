49e863b0eeab0da0344ccf465ac5296668322648
jeff14994 <jeff14994@gmail.com>
2021-05-25 10:58:18 +0800

    Fix signIn.py to make real requests

--- signIn.py
+++ signIn.py
@@ -9,41 +9,13 @@ from datetime import datetime
 load_dotenv()
 qrcode = os.environ.get('QRCODE')
 student_name = os.environ.get('NAME')
-# print(qrcode)
-# print(student_name)
+
 # Start request
 url = 'https://manage.iiiedu.org.tw/api/class/remoteAttendance?qrcode=' + qrcode
 res = requests.get(url)
 
-# print(res.text)
-response = """
-<style type="text/css">
-bg-img {
-    background: url('/qrcode/remote_attendance.png');
-    background-size:     contain;
-    background-repeat:   no-repeat;
-    background-position: center center;     
-}
-.text {
-    text-align: center;
-    font-size: 50px;
-}
-body{
-    font-family: "Microsoft JhengHei"!important;
-}
-</style>
-<body>
-    <div>
-        <p class="text">課程名稱：AI 人工智慧創新應用就業養成班</p>
-        <p class="text">學生姓名：洪裕權</p>
-        <p class="text">刷卡時間：2021-05-25 07:59:04</p> 
-            </div>
-
-</body>
-"""
 # Response from web
-# soup = BeautifulSoup(res.text, 'html.parser')
-soup = BeautifulSoup(response, 'html.parser')
+soup = BeautifulSoup(res.text, 'html.parser')
 p_tag = soup.find_all('p')
 class_name = 'AI 人工智慧創新應用就業養成班'
 # 資料前處理
@@ -55,7 +27,3 @@ if (response_class == class_name and response_name == student_name):
     print('打卡成功！')
     print(response_time)
     send_email(response_time)
-
-# for i in p_tag:
-#     print(i.text) 
-# print(soup.find_all('p').text)
